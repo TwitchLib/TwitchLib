43a36e094674fba08b0aa8b0d89cee39565bd926
swiftyspiffy <swiftyspiffy@gmail.com>
2015-12-30 20:03:28 -0900

    Updated gitignore to ignore .vs/ directory, and added subscription class.

---
+++ TwitchLib/TwitchAPIClasses/Subscription.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.TwitchAPIClasses
+{
+    class Subscription
+    {
+        private int id;
+        private string logo, name, account_created_at, account_updated_at, display_name, subscription_created_at;
+        private bool staff;
+        
+        public int ID { get { return id; } }
+        public string Logo { get { return logo; } }
+        public string Name { get { return name; } }
+        public string AccountCreatedAt { get { return account_created_at; } }
+        public string AccountUpdatedAt { get { return account_updated_at; } }
+        public string DisplayName { get { return display_name; } }
+        public string SubscriptionCreatedAt { get { return subscription_created_at; } }
+    }
+}
