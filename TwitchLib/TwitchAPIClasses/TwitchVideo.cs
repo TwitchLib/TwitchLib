94b25f9a1247b9fb612afba34e9ba9e959ac050e
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-07-01 01:03:32 -0600

    Fixes a bug that results when a specific FPS is not available or returned in the video JSON object.

--- TwitchLib/TwitchAPIClasses/TwitchVideo.cs
+++ TwitchLib/TwitchAPIClasses/TwitchVideo.cs
@@ -15,36 +15,63 @@ namespace TwitchLib.TwitchAPIClasses
         ResolutionsData _resolutions;
         ChannelData _channel;
 
+        /// <summary>Object representing all channel data returned by this request.</summary>
         public ChannelData Channel => _channel;
+        /// <summary>Object representing the available FPSs of versions of the video (-1 representings property doesnt exist)</summary>
         public FpsData Fps => _fps;
+        /// <summary>Length of video in seconds.</summary>
         public int Length => _length;
+        /// <summary>Number of recorded views.</summary>
         public int Views => _views;
+        /// <summary>All available resolutions of video.</summary>
         public ResolutionsData Resolutions => _resolutions;
+        /// <summary>Unique identifier assigned to broadcast video originated from.</summary>
         public string BroadcastId => _broadcastId;
+        /// <summary>Creator's description of video.</summary>
         public string Description => _description;
+        /// <summary>Game being played in the video.</summary>
         public string Game => _game;
+        /// <summary>Id of the particular video.</summary>
         public string Id => _id;
+        /// <summary>Video preview image link.</summary>
         public string Preview => _preview;
+        /// <summary>Date and time string representing recorded date.</summary>
         public string RecordedAt => _recordedAt;
+        /// <summary>Current status of the recorded video.</summary>
         public string Status => _status;
+        /// <summary>Tags assigned to video either automatically or by content creator.</summary>
         public string TagList => _tagList;
+        /// <summary>Title of video.</summary>
         public string Title => _title;
+        /// <summary>Twitch URL to video.</summary>
         public string Url => _url;
 
         public TwitchVideo(JToken apiResponse)
         {
-            int length, views, audioFps;
-            double mediumFps, mobileFps, highFps, lowFps, chunkedFps;
+            int length = -1;
+            int views = -1;
+            int audioFps = -1;
+            double mediumFps = -1;
+            double mobileFps = -1;
+            double highFps = -1;
+            double lowFps = -1;
+            double chunkedFps = -1;
 
             if (int.TryParse(apiResponse.SelectToken("length").ToString(), out length)) _length = length;
             if (int.TryParse(apiResponse.SelectToken("views").ToString(), out views)) _views = views;
 
-            int.TryParse(apiResponse.SelectToken("fps").SelectToken("audio_only").ToString(), out audioFps);
-            double.TryParse(apiResponse.SelectToken("fps").SelectToken("medium").ToString(), out mediumFps);
-            double.TryParse(apiResponse.SelectToken("fps").SelectToken("mobile").ToString(), out mobileFps);
-            double.TryParse(apiResponse.SelectToken("fps").SelectToken("high").ToString(), out highFps);
-            double.TryParse(apiResponse.SelectToken("fps").SelectToken("low").ToString(), out lowFps);
-            double.TryParse(apiResponse.SelectToken("fps").SelectToken("chunked").ToString(), out chunkedFps);
+            if(apiResponse.SelectToken("fps").SelectToken("audio_only") != null)
+                int.TryParse(apiResponse.SelectToken("fps").SelectToken("audio_only").ToString(), out audioFps);
+            if(apiResponse.SelectToken("fps").SelectToken("medium") != null)
+                double.TryParse(apiResponse.SelectToken("fps").SelectToken("medium").ToString(), out mediumFps);
+            if(apiResponse.SelectToken("fps").SelectToken("mobile") != null)
+                double.TryParse(apiResponse.SelectToken("fps").SelectToken("mobile").ToString(), out mobileFps);
+            if(apiResponse.SelectToken("fps").SelectToken("high") != null)
+                double.TryParse(apiResponse.SelectToken("fps").SelectToken("high").ToString(), out highFps);
+            if(apiResponse.SelectToken("fps").SelectToken("low") != null)
+                double.TryParse(apiResponse.SelectToken("fps").SelectToken("low").ToString(), out lowFps);
+            if(apiResponse.SelectToken("fps").SelectToken("chunked") != null)
+                double.TryParse(apiResponse.SelectToken("fps").SelectToken("chunked").ToString(), out chunkedFps);
 
             _broadcastId = apiResponse.SelectToken("broadcast_id").ToString();
             _description = apiResponse.SelectToken("description").ToString();
