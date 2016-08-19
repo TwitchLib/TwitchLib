906c6a6afc5ec88151ec091a0b1f64fcb702ee89
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-08-18 19:58:59 -0600

    Removes a lot of no longer needed code.

--- TwitchLib/TwitchClientClasses/ConnectionCredentials.cs
+++ TwitchLib/TwitchClientClasses/ConnectionCredentials.cs
@@ -8,62 +8,16 @@ namespace TwitchLib
 {
     public class ConnectionCredentials
     {
-        public enum ClientType
-        {
-            Chat,
-            Whisper
-        }
+        public string TwitchUsername { get; set; }
+        public string TwitchOAuth { get; set; }
 
-        private string _host, _twitchUsername, _twitchOAuth;
-        private int _port;
+        public string TwitchHost { get; set; } = "irc.chat.twitch.tv";
+        public int TwitchPort { get; set; } = 6667;
 
-        public string Host
+        public ConnectionCredentials(string twitchUsername, string twitchOAuth)
         {
-            get { return _host; }
-            set { _host = value; }
-        }
-
-        public string TwitchUsername
-        {
-            get { return _twitchUsername; }
-            set { _twitchUsername = value; }
-        }
-
-        public string TwitchOAuth
-        {
-            get { return _twitchOAuth; }
-            set { _twitchOAuth = value; }
-        }
-
-        public int Port
-        {
-            get { return _port; }
-            set { _port = value; }
-        }
-
-        public ConnectionCredentials(string host, int port, string twitchUsername, string twitchOAuth)
-        {
-            _host = host;
-            _port = port;
-            _twitchUsername = twitchUsername.ToLower();
-            _twitchOAuth = twitchOAuth;
-        }
-
-        public ConnectionCredentials(ClientType type, TwitchIpAndPort tIpAndPort, string twitchUsername,
-            string twitchOAuth)
-        {
-            if (type == ClientType.Chat)
-            {
-                _host = tIpAndPort.GetFirstChatServer().Ip;
-                _port = tIpAndPort.GetFirstChatServer().Port;
-            }
-            else
-            {
-                _host = tIpAndPort.GetFirstWhisperServer().Ip;
-                _port = tIpAndPort.GetFirstWhisperServer().Port;
-            }
-            _twitchUsername = twitchUsername.ToLower();
-            _twitchOAuth = twitchOAuth;
+            TwitchUsername = twitchUsername.ToLower();
+            TwitchOAuth = twitchOAuth;
         }
     }
 }
