703755b6c4a033fcb5897f77b1c7d57c165ac8d1
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-10-26 18:13:27 -0600

    Undoes the hack for 400 responses.

--- TwitchLib/TwitchAPIClasses/FollowersResponse.cs
+++ TwitchLib/TwitchAPIClasses/FollowersResponse.cs
@@ -16,26 +16,15 @@ namespace TwitchLib.TwitchAPIClasses
         public int TotalFollowerCount { get; protected set; }
         /// <summary>Property representing cursor for pagination.</summary>
         public string Cursor { get; protected set; }
-        /// <summary>This call has a tendency to return random 400 response codes, so this will be set to false if 400 received</summary>
-        public bool CallSuccessful { get; protected set; }
         
         /// <summary>FollowersResponse object constructor.</summary>
-        public FollowersResponse(string apiResponse = null)
+        public FollowersResponse(string apiResponse)
         {
-            if(apiResponse == null)
-            {
-                Followers = new List<Follower>();
-                TotalFollowerCount = 0;
-                Cursor = "";
-                CallSuccessful = false;
-                return;
-            }
             JObject json = JObject.Parse(apiResponse);
             foreach (JToken follower in json.SelectToken("follows"))
                 Followers.Add(new Follower(follower));
             TotalFollowerCount = int.Parse(json.SelectToken("_total").ToString());
             Cursor = json.SelectToken("_cursor").ToString();
-            CallSuccessful = true;
         }
     }
 }
