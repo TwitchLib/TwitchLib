cd6e71ad3a72662d92ec97242d543471808b3198
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-11-30 13:26:50 -0700

    Log function added to Common.

--- TwitchLib/Common.cs
+++ TwitchLib/Common.cs
@@ -11,6 +11,46 @@ namespace TwitchLib
     public static class Common
     {
         /// <summary>
+        /// Writes message to console output. Maintains foreground color while applying a temporary color. Locks output to ensure color is applied (may be incorrect way to go about it)
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="includeDate"></param>
+        /// <param name="includeTime"></param>
+        /// <param name="type"></param>
+        public static void Log(string message, bool includeDate = false, bool includeTime = false, Enums.LogType type = Enums.LogType.Normal)
+        {
+            lock (Console.Out)
+            {
+                ConsoleColor prevColor = Console.ForegroundColor;
+                switch (type)
+                {
+                    case Enums.LogType.Normal:
+                        Console.ForegroundColor = ConsoleColor.White;
+                        break;
+                    case Enums.LogType.Success:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case Enums.LogType.Failure:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                }
+                string dateTimeStr = "";
+                if (includeDate && includeTime)
+                    dateTimeStr = $"{DateTime.UtcNow}";
+                else if (includeDate)
+                    dateTimeStr = $"{DateTime.UtcNow.ToShortDateString()}";
+                else
+                    dateTimeStr = $"{DateTime.UtcNow.ToShortTimeString()}";
+
+                if (includeDate || includeTime)
+                    Console.WriteLine($"[TwitchLib - {dateTimeStr}] {message}");
+                else
+                    Console.WriteLine($"[TwitchLib] {message}");
+                Console.ForegroundColor = prevColor;
+            }
+        }
+
+        /// <summary>
         /// Function to check if a jtoken is null.
         /// Credits: http://stackoverflow.com/questions/24066400/checking-for-empty-null-jtoken-in-a-jobject
         /// </summary>
