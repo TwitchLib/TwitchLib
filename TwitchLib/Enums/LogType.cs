1f55556f27ce59bf0643f225fcbdee113122a4bb
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2016-11-30 13:29:23 -0700

    Enum LogType added for Log function.

---
+++ TwitchLib/Enums/LogType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Enums
+{
+    /// <summary>
+    /// Enum to alter color effects in log
+    /// </summary>
+    public enum LogType
+    {
+        Normal,
+        Failure,
+        Success
+    }
+}
