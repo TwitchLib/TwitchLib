a5d344dc0c17a583c7dc8474209b805d8900e0df
swiftyspiffy <swiftyspiffy@gmail.com>
2016-12-25 15:03:53 -0900

    Adds OnNowHostingArgs class.

---
+++ TwitchLib/Events/Client/OnNowHostingArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Events.Client
+{
+    /// <summary>Args representing the detected hosted channel.</summary>
+    public class OnNowHostingArgs : EventArgs
+    {
+        /// <summary>Property the channel that received the event.</summary>
+        public string Channel;
+        /// <summary>Property representing channel that is being hosted.</summary>
+        public string HostedChannel;
+    }
+}
