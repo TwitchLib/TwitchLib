360c0360a144cd3bedf2ab1511b60bdc59ad2e89
swiftyspiffy <swiftyspiffy@gmail.com>
2016-12-29 03:22:45 -0900

    Badges is now a List of KeyValuePair objects instead of a string. Color property added to WhisperMessage represents color object of the sender's name. MessageId and UserId are long data types for safety. Cleaned up parsing for WhisperMessage.

--- TwitchLib/Models/Client/WhisperMessage.cs
+++ TwitchLib/Models/Client/WhisperMessage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,11 @@ namespace TwitchLib.Models.Client
     public class WhisperMessage
     {
         /// <summary>Property representing dynamic badges assigned to message.</summary>
-        public string Badges { get; protected set; }
+        public List<KeyValuePair<string, string>> Badges { get; protected set; }
         /// <summary>Property representing HEX representation of color of username.</summary>
         public string ColorHex { get; protected set; }
+        /// <summary>Property representing HEX color as a System.Drawing.Color object.</summary>
+        public Color Color { get; protected set; }
         /// <summary>Property representing sender Username.</summary>
         public string Username { get; protected set; }
         /// <summary>Property representing sender DisplayName (can be null/blank).</summary>
@@ -24,9 +27,9 @@ namespace TwitchLib.Models.Client
         /// <summary>Property representing identifier of the message thread.</summary>
         public string ThreadId { get; protected set; }
         /// <summary>Property representing message identifier.</summary>
-        public int MessageId { get; protected set; }
+        public long MessageId { get; protected set; }
         /// <summary>Property representing sender identifier.</summary>
-        public int UserId { get; protected set; }
+        public long UserId { get; protected set; }
         /// <summary>Property representing whether or not sender has Turbo.</summary>
         public bool Turbo { get; protected set; }
         /// <summary>Property representing bot's username.</summary>
@@ -43,45 +46,65 @@ namespace TwitchLib.Models.Client
         /// <param name="botUsername">Active bot username receiving message.</param>
         public WhisperMessage(string ircString, string botUsername)
         {
-            if (ircString.Substring(0, 8) == "@badges=")
-            {
-                //@badges=;color=#00FF7F;display-name=Dara226;emotes=;message-id=53;thread-id=62192703_66137196;turbo=0;user-id=62192703;user-type= :dara226!dara226@dara226.tmi.twitch.tv WHISPER the_kraken_bot :ahoy
-                Badges = ircString.Split(';')[0].Split('=')[1];
-                ColorHex = ircString.Split(';')[1].Split('=')[1];
-                Username = ircString.Split('@')[2].Split('.')[0];
-                DisplayName = ircString.Split(';')[2].Split('=')[1];
-                MessageId = int.Parse(ircString.Split(';')[4].Split('=')[1]);
-                ThreadId = ircString.Split(';')[5].Split('=')[1];
-                Turbo = ConvertToBool(ircString.Split(';')[6].Split('=')[1]);
-                UserId = int.Parse(ircString.Split(';')[7].Split('=')[1]);
-                // UserType should be converted to an enum value I think at somepoint
-                UserType = ircString.Split(';')[8].Split('=')[1].Replace(" ", "");
-                Message =
-                    ircString.Replace(
-                        ircString.Split('@')[0] + "@" + ircString.Split('@')[1] + "@" +
-                        ircString.Split('@')[2].Split(':')[0] + ":", string.Empty);
-                EmoteSet = new EmoteSet(ircString.Split(';')[3].Split('=')[1], Message);
-            }
-            else
-            {
-                //@color=#00FF7F;display-name=Dara226;emotes=;message-id=53;thread-id=62192703_66137196;turbo=0;user-id=62192703;user-type= :dara226!dara226@dara226.tmi.twitch.tv WHISPER the_kraken_bot :ahoy
-                Badges = "";
-                ColorHex = ircString.Split(';')[0].Split('=')[1];
-                Username = ircString.Split('@')[2].Split('.')[0];
-                DisplayName = ircString.Split(';')[1].Split('=')[1];
-                MessageId = int.Parse(ircString.Split(';')[3].Split('=')[1]);
-                ThreadId = ircString.Split(';')[4].Split('=')[1];
-                Turbo = ConvertToBool(ircString.Split(';')[5].Split('=')[1]);
-                UserId = int.Parse(ircString.Split(';')[6].Split('=')[1]);
-                // UserType should be converted to an enum value I think at somepoint
-                UserType = ircString.Split(';')[7].Split('=')[1].Replace(" ", "");
-                Message =
-                    ircString.Replace(
-                        ircString.Split('@')[0] + "@" + ircString.Split('@')[1] + "@" +
-                        ircString.Split('@')[2].Split(':')[0] + ":", string.Empty);
-                EmoteSet = new EmoteSet(ircString.Split(';')[2].Split('=')[1], Message);
-            }
+            Username = ircString.Split('!')[1].Split('@')[0];
             BotUsername = botUsername;
+            Message = ircString.Replace($"{ircString.Split('!')[0]}!{Username}@{Username}.tmi.twitch.tv WHISPER {botUsername.ToLower()} :", "");
+
+            foreach(string part in ircString.Split(';'))
+            {
+                string key = part.Split('=')[0];
+                string value = part.Split('=')[1];
+                switch(key)
+                {
+                    
+                    case "@badges":
+                        Badges = new List<KeyValuePair<string, string>>();
+                        if (value.Contains('/'))
+                        {
+                            if (!value.Contains(","))
+                                Badges.Add(new KeyValuePair<string, string>(value.Split('/')[0], value.Split('/')[1]));
+                            else
+                                foreach (string badge in value.Split(','))
+                                    Badges.Add(new KeyValuePair<string, string>(badge.Split('/')[0], badge.Split('/')[1]));
+                        }
+                        break;
+
+                    case "color":
+                        ColorHex = value;
+                        if (!string.IsNullOrEmpty(ColorHex))
+                            Color = ColorTranslator.FromHtml(ColorHex);
+                        break;
+
+                    case "display-name":
+                        DisplayName = value;
+                        break;
+
+                    case "emotes":
+                        EmoteSet = new EmoteSet(value, Message);
+                        break;
+
+                    case "message-id":
+                        MessageId = long.Parse(value);
+                        break;
+
+                    case "thread-id":
+                        ThreadId = value;
+                        break;
+
+                    case "turbo":
+                        if (value == "1")
+                            Turbo = true;
+                        break;
+
+                    case "user-id":
+                        UserId = long.Parse(value);
+                        break;
+
+                    default:
+                        Common.Log($"Unaccounted for [WhisperMessage.cs]: key: {key}, value: {value}");
+                        break;
+                }
+            }
         }
 
         private bool ConvertToBool(string data)
