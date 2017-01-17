1b12745342b49ba9be271369633dc38182b5b3cd
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-01-17 00:48:55 -0700

    New Chat parsing class.

--- TwitchLib/Internal/Parsing/Chat.cs
+++ TwitchLib/Internal/Parsing/Chat.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using TwitchLib.Models.Client;
 
 namespace TwitchLib.Internal.Parsing
 {
