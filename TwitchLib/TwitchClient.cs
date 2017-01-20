b697d6eacc84ba676146424de0aa8bc045a51e16
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-01-19 20:29:02 -0700

    Auto join mechanism now forces channel to lowercase.

--- TwitchLib/TwitchClient.cs
+++ TwitchLib/TwitchClient.cs
@@ -235,7 +235,7 @@ namespace TwitchLib
                 Common.Logging.Log($"TwitchLib-TwitchClient initialized, assembly version: {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}");
             _credentials = credentials;
             TwitchUsername = _credentials.TwitchUsername;
-            _autoJoinChannel = channel;
+            _autoJoinChannel = channel.ToLower();
             if(chatCommandIdentifier != '\0')
                 _chatCommandIdentifiers.Add(chatCommandIdentifier);
             if (whisperCommandIdentifier != '\0')
