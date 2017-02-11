8c42d2d4e594e8ea0c9a19262f1d774661d92153
Rafal Tracilowski <rafal@tracilowski.pl>
2017-02-11 00:13:20 +0000

    Unit test of change

---
+++ TwitchLibTests/TwitchClientTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwitchLib.Models.Client;
+
+namespace TwitchLib.Tests
+{
+    [TestClass()]
+    public class TwitchClientTests
+    {
+        private readonly ConnectionCredentials TestCredentials = new ConnectionCredentials("test", "oauth:1");
+
+        [TestMethod()]
+        public void TwitchClient_InitializeWithoutChannel()
+        {
+            var client = new TwitchClient(TestCredentials);
+
+            Assert.IsNotNull(client, "Expected client to be initialized");
+        }
+    }
+}
