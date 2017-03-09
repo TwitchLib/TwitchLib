e398c32b82b685eb6c685bf701a55a2e4a4b9c27
Steven Hall <vaindil@gmail.com>
2017-03-08 21:42:38 -0500

    v5 user ID should be string

--- TwitchLib/Models/API/v5/User.cs
+++ TwitchLib/Models/API/v5/User.cs
@@ -21,7 +21,7 @@ namespace TwitchLib.Models.API.v5
         /// <summary>Property representing the logo of the user.</summary>
         public string Logo { get; protected set; }
         /// <summary>Property representing the Id of the user.</summary>
-        public long Id { get; protected set; }
+        public string Id { get; protected set; }
         /// <summary>Property representing the bio of the user.</summary>
         public string Bio { get; protected set; }
 
@@ -34,7 +34,7 @@ namespace TwitchLib.Models.API.v5
             CreatedAt = Common.Helpers.DateTimeStringToObject(json.SelectToken("created_at")?.ToString());
             UpdatedAt = Common.Helpers.DateTimeStringToObject(json.SelectToken("updated_at")?.ToString());
             Logo = json.SelectToken("logo")?.ToString();
-            Id = (json.SelectToken("_id") != null) ? long.Parse(json.SelectToken("_id").ToString()) : -1;
+            Id = json.SelectToken("_id")?.ToString();
             Bio = json.SelectToken("bio")?.ToString();
         }
     }
