9a9e14615f35fd609a126f070199db6dfae9cc99
Ethan Lu <elu@nevada.unr.edu>
2017-03-14 14:15:54 -0700

    Oops+Created Exception

--- TwitchLib/Events/Services/LiveChannelMonitor/OnStreamMonitorEndedArgs.cs
+++ TwitchLib/Events/Services/LiveChannelMonitor/OnStreamMonitorEndedArgs.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwitchLib.Models.Client;
-using TwitchLib.Enums
+using TwitchLib.Enums;
 
 namespace TwitchLib.Events.Services.LiveStreamMonitor
 {
---
+++ TwitchLib/Exceptions/Services/UnitializedChannelList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Exceptions.Services
+{
+    /// <summary>Exception representing no channel list being provided.</summary>
+    public class UnintializedChannelList : Exception
+    {
+        /// <summary>Exception constructor.</summary>
+        public UnintializedChannelList(string data)
+            : base(data)
+        {
+        }
+    }
+}
