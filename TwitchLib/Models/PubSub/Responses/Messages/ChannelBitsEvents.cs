4ba2c9bb5667d413066281f77d1548710c37ef39
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-03-16 17:02:33 -0600

    Adds comments to XML comments.

--- TwitchLib/Models/PubSub/Responses/Messages/ChannelBitsEvents.cs
+++ TwitchLib/Models/PubSub/Responses/Messages/ChannelBitsEvents.cs
@@ -7,18 +7,29 @@ using Newtonsoft.Json.Linq;
 
 namespace TwitchLib.Models.PubSub.Responses.Messages
 {
+    /// <summary>Model representing the data in a channel bits event.</summary>
     public class ChannelBitsEvents : MessageData
     {
+        /// <summary>Username of the sender.</summary>
         public string Username { get; protected set; }
+        /// <summary>The channel the bits were sent to.</summary>
         public string ChannelName { get; protected set; }
+        /// <summary>User ID of the sender.</summary>
         public string UserId { get; protected set; }
+        /// <summary>Channel/User ID of where the bits were sent to.</summary>
         public string ChannelId { get; protected set; }
+        /// <summary>Time stamp of the event.</summary>
         public string Time { get; protected set; }
+        /// <summary>Chat message that accompanied the bits.</summary>
         public string ChatMessage { get; protected set; }
+        /// <summary>The amount of bits sent.</summary>
         public int BitsUsed { get; protected set; }
+        /// <summary>The total amount of bits the user has sent.</summary>
         public int TotalBitsUsed { get; protected set; }
+        /// <summary>Context related to event.</summary>
         public string Context { get; protected set; }
 
+        /// <summary>ChannelBitsEvent model constructor.</summary>
         public ChannelBitsEvents(string jsonStr)
         {
             JToken json = JObject.Parse(jsonStr);
