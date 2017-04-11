562c336b08ca48c11f1f7bc06202fedfdb4b3428
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-04-11 16:24:34 -0600

    further cleanup

--- TwitchLib/Models/API/v3/Channels/GetTeamsResponse.cs
+++ TwitchLib/Models/API/v3/Channels/GetTeamsResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace TwitchLib.Models.API.v3.Channels
 {
     public class GetTeamsResponse
     {
+        [JsonProperty(PropertyName = "teams")]
         public Teams.Team[] Teams;
     }
 }
