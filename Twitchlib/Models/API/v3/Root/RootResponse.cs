cbcfb227d03bf850fb6b7f2279c5fbffaad5d681
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-04-13 02:27:35 -0600

    Root v3 endpoints done.

---
+++ Twitchlib/Models/API/v3/Root/RootResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Models.API.v3.Root
+{
+    public class RootResponse
+    {
+        [JsonProperty(PropertyName = "identified")]
+        public bool Identified { get; protected set; }
+        [JsonProperty(PropertyName = "token")]
+        public Token Token { get; protected set; }
+    }
+}
