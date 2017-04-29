47949cdb49fdfa828902e4d282397b61154a913d
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-04-29 15:55:26 -0600

    Added undocumented function GetChannelHosts.

---
+++ Twitchlib/Models/API/Undocumented/Hosting/ChannelHostsResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Models.API.Undocumented.Hosting
+{
+    public class ChannelHostsResponse
+    {
+        [JsonProperty(PropertyName = "hosts")]
+        public HostListing[] Hosts { get; protected set; }
+    }
+}
---
+++ Twitchlib/Models/API/Undocumented/Hosting/HostListing.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Models.API.Undocumented.Hosting
+{
+    public class HostListing
+    {
+        [JsonProperty(PropertyName = "host_id")]
+        public string HostId { get; protected set; }
+        [JsonProperty(PropertyName = "target_id")]
+        public string TargetId { get; protected set; }
+        [JsonProperty(PropertyName = "host_login")]
+        public string HostLogin { get; protected set; }
+        [JsonProperty(PropertyName = "target_login")]
+        public string TargetLogin { get; protected set; }
+        [JsonProperty(PropertyName = "host_display_name")]
+        public string HostDisplayName { get; protected set; }
+        [JsonProperty(PropertyName = "target_display_name")]
+        public string TargetDisplayName { get; protected set; }
+    }
+}
