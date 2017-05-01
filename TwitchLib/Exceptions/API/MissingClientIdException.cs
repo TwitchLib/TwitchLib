04126d0cbaa0ab53a458e7d2a679f8dc6c5e59ef
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-05-01 16:27:10 -0600

    Adds web exception for missing clientid

---
+++ TwitchLib/Exceptions/API/MissingClientIdException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Exceptions.API
+{
+    /// <summary>Exception representing a request that doesn't have a clientid attached.</summary>
+    public class MissingClientIdException : Exception
+    {
+        /// <summary>Exception constructor</summary>
+        public MissingClientIdException(string apiData)
+            : base(apiData)
+        {
+        }
+    }
+}
