60918f1a1c470538f94a467c206934d0b968d9cc
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-05-01 20:19:18 -0600

    All in the name of the FollowerService.

--- TwitchLib/Events/Services/FollowerService/OnServiceStartedArgs.cs
+++ TwitchLib/Events/Services/FollowerService/OnServiceStartedArgs.cs
@@ -6,8 +6,10 @@
     /// <summary>Class representing event args for OnServiceStarted event.</summary>
     public class OnServiceStartedArgs : EventArgs
     {
+        /// <summary>Event property representing whether channeldata is a channel name or channel Id.</summary>
+        public Enums.ChannelIdentifierType ChannelIdentifier;
         /// <summary>Event property representing channel the service is currently monitoring.</summary>
-        public string Channel;
+        public string ChannelData;
         /// <summary>Event property representing number of recent followers a query to Twitch Api should return.</summary>
         public int QueryCount;
         /// <summary>Event property representing seconds between queries to Twitch Api.</summary>
--- TwitchLib/Events/Services/FollowerService/OnServiceStoppedArgs.cs
+++ TwitchLib/Events/Services/FollowerService/OnServiceStoppedArgs.cs
@@ -6,8 +6,10 @@
     /// <summary>Class representing event args for OnServiceStopped event.</summary>
     public class OnServiceStoppedArgs : EventArgs
     {
+        /// <summary>Event property representing whether channel data is a channel name or a channel id.</summary>
+        public Enums.ChannelIdentifierType ChannelIdentifier;
         /// <summary>Event property representing channel the service is currently monitoring.</summary>
-        public string Channel;
+        public string ChannelData;
         /// <summary>Event property representing number of recent followers a query to Twitch Api should return.</summary>
         public int QueryCount;
         /// <summary>Event property representing seconds between queries to Twitch Api.</summary>
