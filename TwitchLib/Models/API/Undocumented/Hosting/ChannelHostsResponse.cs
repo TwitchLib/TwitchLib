70f6892dd4d3bca21cfa82228e19e1e3e1922a80
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-05-01 19:44:10 -0600

    Adds missing files.

---
+++ TwitchLib/Models/API/Undocumented/Hosting/ChannelHostsResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TwitchLib.Models.API.Undocumented.Hosting
+{
+    #region using directives
+    using Newtonsoft.Json;
+    #endregion
+    public class ChannelHostsResponse
+    {
+        [JsonProperty(PropertyName = "hosts")]
+        public HostListing[] Hosts { get; protected set; }
+    }
+}
