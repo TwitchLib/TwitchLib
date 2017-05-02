c89fa96e2d683ef611a3011c41f8031d5f4cca4e
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-05-01 20:32:13 -0600

    Readds missing file.

---
+++ TwitchLib/Exceptions/Services/UnitializedChannelListException.cs
@@ -0,0 +1,15 @@
+﻿namespace TwitchLib.Exceptions.Services
+{
+    #region using directives
+    using System;
+    #endregion
+    /// <summary>Exception representing no channel list being provided.</summary>
+    public class UnintializedChannelList : Exception
+    {
+        /// <summary>Exception constructor.</summary>
+        public UnintializedChannelList(string data)
+            : base(data)
+        {
+        }
+    }
+}
