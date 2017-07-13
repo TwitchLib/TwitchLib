0bf652740b0f97739b519cf277b260ed98a1408e
Adam Michaud <invariel@gmail.com>
2017-07-13 03:14:36 -0400

    Bit Event Update and Listen Changes
    Bit Event updated from v3 to v5 (or from no version to v1)
    
    Changed how Listen works because you listen to a list of events all at
    once with the same Oauth key.

--- TwitchLib/Models/PubSub/Responses/Messages/ChannelBitsEvents.cs
+++ TwitchLib/Models/PubSub/Responses/Messages/ChannelBitsEvents.cs
@@ -24,20 +24,39 @@
         public int TotalBitsUsed { get; protected set; }
         /// <summary>Context related to event.</summary>
         public string Context { get; protected set; }
+        /// <summary>Next badge at value</summary>
+        public int NewBadgeEntitlement { get; protected set; }
+        /// <summary>Previous badge received at value</summary>
+        public int PreviousBadgeEntitlement { get; protected set; }
 
         /// <summary>ChannelBitsEvent model constructor.</summary>
         public ChannelBitsEvents(string jsonStr)
         {
+
+
+            /*
+                    JObject message = JObject.Parse((String)json["data"]["message"]);
+
+                    String username = (String)message["data"]["user_name"];
+                    String chat_message = (String)message["data"]["chat_message"];
+                    int bits = (Int32)message["data"]["bits_used"];
+
+                    // Then, if bits > bitsThreshold, create an event.
+                    String displayMessage = String.Format("{0} donated {1} bits.", username, bits);
+             */
+
             JToken json = JObject.Parse(jsonStr);
-            Username = json.SelectToken("user_name")?.ToString();
-            ChannelName = json.SelectToken("channel_name")?.ToString();
-            UserId = json.SelectToken("user_id")?.ToString();
-            ChannelId = json.SelectToken("channel_id")?.ToString();
-            Time = json.SelectToken("time")?.ToString();
-            ChatMessage = json.SelectToken("chat_message")?.ToString();
-            BitsUsed = int.Parse(json.SelectToken("bits_used").ToString());
-            TotalBitsUsed = int.Parse(json.SelectToken("total_bits_used").ToString());
-            Context = json.SelectToken("context")?.ToString();
+            Username = json.SelectToken("data").SelectToken("user_name")?.ToString();
+            ChannelName = json.SelectToken("data").SelectToken("channel_name")?.ToString();
+            UserId = json.SelectToken("data").SelectToken("user_id")?.ToString();
+            ChannelId = json.SelectToken("data").SelectToken("channel_id")?.ToString();
+            Time = json.SelectToken("data").SelectToken("time")?.ToString();
+            ChatMessage = json.SelectToken("data").SelectToken("chat_message")?.ToString();
+            BitsUsed = int.Parse(json.SelectToken("data").SelectToken("bits_used").ToString());
+            TotalBitsUsed = int.Parse(json.SelectToken("data").SelectToken("total_bits_used").ToString());
+            Context = json.SelectToken("data").SelectToken("context")?.ToString();
+            NewBadgeEntitlement = int.Parse(json.SelectToken("data").SelectToken("badge_entitlement").SelectToken("new_version").ToString());
+            PreviousBadgeEntitlement = int.Parse(json.SelectToken("data").SelectToken("badge_entitlement").SelectToken("previous_version").ToString());
         }
     }
 }
