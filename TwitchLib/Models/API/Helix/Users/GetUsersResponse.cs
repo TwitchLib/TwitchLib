df64cda5d198c42ef18f7d86ff2aa85da9745f65
swiftyspiffy <swiftyspiffy@gmail.com>
2017-09-14 17:33:44 -0800

    Fixed file structure related to helix.

---
+++ TwitchLib/Models/API/Helix/Users/GetUsersResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchLib.Models.API.Helix.Users
+{
+    public class GetUsersResponse
+    {
+        [JsonProperty(PropertyName = "data")]
+        public User[] Users { get; protected set; }
+    }
+}
