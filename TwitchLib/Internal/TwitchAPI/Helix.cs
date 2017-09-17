7ea64be3b205b8eedb60124d45e412bcda5c634c
swiftyspiffy <swiftyspiffy@gmail.com>
2017-09-17 15:21:29 -0800

    Support for Helix GetStreams endpoint.

--- TwitchLib/Internal/TwitchAPI/Helix.cs
+++ TwitchLib/Internal/TwitchAPI/Helix.cs
@@ -46,7 +46,34 @@ namespace TwitchLib.Internal.TwitchAPI
 
             public async static Task PutUsers(string description, string accessToken = null)
             {
-                await Requests.PutAsync($"https://api.twitch.tv/helix/users?description={description}", null, accessToken);
+                await Requests.PutAsync($"https://api.twitch.tv/helix/users?description={description}", null, accessToken, Requests.API.Helix);
+            }
+        }
+
+        public static class Streams
+        {
+            public async static Task<Models.API.Helix.Streams.GetStreams.GetStreamsResponse> GetStreams(string after = null, List<string> communityIds = null, int first = 20, List<string> gameIds = null, List<string> languages = null, string type = "all", List<string> userIds = null, List<string> userLogins = null)
+            {
+                string getParams = $"?first={first}&type={type}";
+                if (after != null)
+                    getParams += $"&after={after}";
+                if (communityIds != null && communityIds.Count > 0)
+                    foreach (var communityId in communityIds)
+                        getParams += $"&community_id={communityId}";
+                if (gameIds != null && gameIds.Count > 0)
+                    foreach (var gameId in gameIds)
+                        getParams += $"&game_id={gameId}";
+                if (languages != null && languages.Count > 0)
+                    foreach (var language in languages)
+                        getParams += $"&language={language}";
+                if (userIds != null && userIds.Count > 0)
+                    foreach (var userId in userIds)
+                        getParams += $"&user_id={userId}";
+                if (userLogins != null && userLogins.Count > 0)
+                    foreach (var userLogin in userLogins)
+                        getParams += $"&user_login={userLogin}";
+
+                return await Requests.GetGenericAsync<Models.API.Helix.Streams.GetStreams.GetStreamsResponse>($"https://api.twitch.tv/helix/streams{getParams}", api: Requests.API.Helix);
             }
         }
     }
