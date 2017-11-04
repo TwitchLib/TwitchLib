844807559b7049cf87e0c350b9cdce23ecded5cb
swiftyspiffy (cole) <swiftyspiffy@gmail.com>
2017-11-04 15:59:18 -0700

    support for helix games endpoint

--- TwitchLib/Api/Sections/Games.cs
+++ TwitchLib/Api/Sections/Games.cs
@@ -13,10 +13,12 @@
         {
             v3 = new V3(api);
             v5 = new V5(api);
+            helix = new Helix(api);
         }
 
         public V3 v3 { get; }
         public V5 v5 { get; }
+        public Helix helix { get; }
 
         public class V3 : ApiSection
         {
@@ -59,5 +61,45 @@
             }
             #endregion
         }
+
+        public class Helix : ApiSection
+        {
+            public Helix(TwitchAPI api) : base(api)
+            {
+            }
+            #region GetGames
+            public async Task<Models.API.v5.Games.TopGames> GetTopGamesAsync(List<string> gameIds = null, List<string> gameNames = null)
+            {
+                if ((gameIds == null && gameNames == null) ||
+                    (gameIds != null && gameIds.Count == 0 && gameNames == null) ||
+                    (gameNames != null && gameNames.Count == 0 && gameIds == null))
+                    throw new Exceptions.API.BadParameterException("Either gameIds or gameNames must have at least one value");
+                if (gameIds != null && gameIds.Count > 100)
+                    throw new Exceptions.API.BadParameterException("gameIds list cannot exceed 100 items");
+                if (gameNames != null && gameNames.Count > 100)
+                    throw new Exceptions.API.BadParameterException("gameNames list cannot exceed 100 items");
+
+                List<KeyValuePair<string, string>> queryParameters = new List<KeyValuePair<string, string>>();
+                if (gameIds != null && gameIds.Count > 0)
+                    foreach (var gameId in gameIds)
+                        queryParameters.Add(new KeyValuePair<string, string>("id", gameId));
+                if (gameNames != null && gameNames.Count > 0)
+                    foreach (var gameName in gameNames)
+                        queryParameters.Add(new KeyValuePair<string, string>("name", gameName));
+
+                string optionalQuery = string.Empty;
+                if (queryParameters.Count > 0)
+                {
+                    for (int i = 0; i < queryParameters.Count; i++)
+                    {
+                        if (i == 0) { optionalQuery = $"?{queryParameters[i].Key}={queryParameters[i].Value}"; }
+                        else { optionalQuery += $"&{queryParameters[i].Key}={queryParameters[i].Value}"; }
+                    }
+                }
+                return await Api.GetGenericAsync<Models.API.v5.Games.TopGames>($"https://api.twitch.tv/helix/games{optionalQuery}", null, ApiVersion.Helix).ConfigureAwait(false);
+            }
+            #endregion
+        }
+
     }
 }
