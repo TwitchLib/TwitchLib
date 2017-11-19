e11a3706455530cfee15015d3091c93a4ea79f33
Tobias Teske <syzuna@t-teske.de>
2017-11-19 20:44:48 +0100

    Added TokenExpiredException

---
+++ TwitchLib/Exceptions/API/TokenExpiredException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TwitchLib.Exceptions.API
+{
+    #region using directives
+    using System;
+    #endregion
+    /// <summary>Exception representing a detection that the OAuth token expired</summary>
+    public class TokenExpiredException : Exception
+    {
+        /// <summary>Exception constructor</summary>
+        public TokenExpiredException(string data)
+            : base(data)
+        {
+        }
+    }
+}
