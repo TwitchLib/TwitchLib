3cebafb7f488a625996d1aac7a68b73d4c8584af
Tobias Teske <syzuna@t-teske.de>
2017-11-26 18:07:32 +0100

    Fixed Auth section and added Refresh Response

--- TwitchLib/Api/Sections/Auth.cs
+++ TwitchLib/Api/Sections/Auth.cs
@@ -1,16 +1,17 @@
 ﻿namespace TwitchLib
 {
+
     #region using directives
     using System.Threading.Tasks;
-    using TwitchLib.Api;
-    using TwitchLib.Enums;
+    using Api;
+    using System.Collections.Generic;
     #endregion
 
-    public class Badges
+    public class Auth
     {
         public V5 v5 { get; }
 
-        public Badges(TwitchAPI api)
+        public Auth(TwitchAPI api)
         {
             v5 = new V5(api);
         }
@@ -21,18 +22,30 @@
             {
             }
 
-            #region GetSubscriberBadgesForChannel
-            public async Task<Models.API.v5.Badges.ChannelDisplayBadges> GetSubscriberBadgesForChannelAsync(string channelId)
+            #region GetFreshToken
+            /// <summary>
+            /// <para>[ASYNC] Refreshes an expired auth token</para>
+            /// <para>ATTENTION: Client Secret required. Never expose it to consumers!</para>
+            /// <para>Throws a BadRequest Exception if the request fails due to a bad refresh token</para>
+            /// </summary>
+            /// <returns>A RefreshResponse object that holds your new auth and refresh token and the list of scopes for that token</returns>
+            public async Task<Models.API.v5.Auth.RefreshResponse> RefreshAuthTokenAsync(string refreshToken, string clientSecret, string clientId = null)
             {
-                if (string.IsNullOrWhiteSpace(channelId)) { throw new Exceptions.API.BadParameterException("The channel id is not valid. It is not allowed to be null, empty or filled with whitespaces."); }
-                return await Api.GetGenericAsync<Models.API.v5.Badges.ChannelDisplayBadges>($"https://badges.twitch.tv/v1/badges/channels/{channelId}/display", null, null, ApiVersion.v5).ConfigureAwait(false);
-            }
-            #endregion
+                var internalClientId = clientId ?? Api.Settings.ClientId;
 
-            #region GetGlobalBadges
-            public async Task<Models.API.v5.Badges.GlobalBadgesResponse> GetGlobalBadgesAsync()
-            {
-                return await Api.GetGenericAsync<Models.API.v5.Badges.GlobalBadgesResponse>("https://badges.twitch.tv/v1/badges/global/display", null, null, ApiVersion.v5).ConfigureAwait(false);
+                if (string.IsNullOrWhiteSpace(refreshToken)) { throw new Exceptions.API.BadParameterException("The refresh token is not valid. It is not allowed to be null, empty or filled with whitespaces."); }
+                if (string.IsNullOrWhiteSpace(clientSecret)) { throw new Exceptions.API.BadParameterException("The client secret is not valid. It is not allowed to be null, empty or filled with whitespaces."); }
+                if (string.IsNullOrWhiteSpace(internalClientId)) { throw new Exceptions.API.BadParameterException("The clientId is not valid. It is not allowed to be null, empty or filled with whitespaces."); }
+
+                var getParams = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string> ("grant_type", "refresh_token"),
+                    new KeyValuePair<string, string> ("refresh_token", refreshToken),
+                    new KeyValuePair<string, string> ("client_id", internalClientId),
+                    new KeyValuePair<string, string> ("client_secret", clientSecret)
+                };
+
+                return await Api.PostGenericAsync<Models.API.v5.Auth.RefreshResponse>("https://api.twitch.tv/kraken/oauth2/token", null, getParams);
             }
             #endregion
         }
