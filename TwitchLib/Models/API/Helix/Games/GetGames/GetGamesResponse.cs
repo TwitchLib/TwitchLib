9d6017e3a35a1be5d99527dca309693d300906ae
Tobias Teske <syzuna@t-teske.de>
2018-02-13 18:57:31 +0100

    Removed unused imports in Helix API Models

--- TwitchLib/Models/API/Helix/Games/GetGames/GetGamesResponse.cs
+++ TwitchLib/Models/API/Helix/Games/GetGames/GetGamesResponse.cs
@@ -1,7 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TwitchLib.Models.API.Helix.Games.GetGames
 {
--- TwitchLib/Models/API/Helix/StreamsMetadata/Overwatch.cs
+++ TwitchLib/Models/API/Helix/StreamsMetadata/Overwatch.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwitchLib.Models.API.Helix.StreamsMetadata
+﻿namespace TwitchLib.Models.API.Helix.StreamsMetadata
 {
     public class Overwatch
     {
