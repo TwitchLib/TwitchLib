9f21b675d972335ce02a6885c8fd3fa0fdcd9af1
B D <26707967+dangbee@users.noreply.github.com>
2018-02-16 22:39:28 +1030

    Add Total Follows

--- TwitchLib/Models/API/Helix/Users/GetUsersFollows/GetUsersFollowsResponse.cs
+++ TwitchLib/Models/API/Helix/Users/GetUsersFollows/GetUsersFollowsResponse.cs
@@ -14,5 +14,7 @@ namespace TwitchLib.Models.API.Helix.Users.GetUsersFollows
         public Follow[] Follows { get; protected set; }
         [JsonProperty(PropertyName = "pagination")]
         public Pagination Pagination { get; protected set; }
+        [JsonProperty(PropertyName = "total")]
+        public long TotalFollows { get; protected set; }
     }
 }
