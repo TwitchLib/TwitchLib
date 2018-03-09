c02c690c32dcf549efbdb01791e4a210d2fe3b83
fossilz <fossilz@treyza.com>
2018-03-08 19:56:38 -0600

    update build file

--- travis-build.sh
+++ travis-build.sh
@@ -1,4 +1,4 @@
 dotnet restore
-dotnet build ./TwitchLib/TwitchLib.Api/TwitchLib.Api/TwitchLib.Api.csproj --framework netstandard2.0
-dotnet build ./TwitchLib/TwitchLib.Client/TwitchLib.Client/TwitchLib.Client.csproj --framework netstandard2.0
-dotnet build ./TwitchLib/TwitchLib.PubSub/TwitchLib.PubSub/TwitchLib.PubSub.csproj --framework netstandard2.0
+dotnet build ./TwitchLib/TwitchLib.Api/TwitchLib.Api/ --framework netstandard2.0
+dotnet build ./TwitchLib/TwitchLib.Client/TwitchLib.Client/ --framework netstandard2.0
+dotnet build ./TwitchLib/TwitchLib.PubSub/TwitchLib.PubSub/ --framework netstandard2.0
